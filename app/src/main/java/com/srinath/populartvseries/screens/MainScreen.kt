56539a8be3a9c6e15737ca034c262ffc4df3ffa4
srinathdev-techjays <srinath.a@techjays.com>
2024-08-02 00:39:50 +0530

    Grid UI Completed

--- app/src/main/java/com/srinath/populartvseries/screens/MainScreen.kt
+++ app/src/main/java/com/srinath/populartvseries/screens/MainScreen.kt
@@ -10,14 +10,21 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyGridState
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
@@ -38,50 +45,127 @@ fun MainScreen(viewModel: TvSeriesViewModel, navController: NavController) {
     val context = LocalContext.current
     val gridState = rememberLazyGridState()
 
-    Box(modifier = Modifier.fillMaxSize().background(color = Color.Black)) {
-        Column(modifier = Modifier.padding(16.dp)) {
-            SearchBar()
-            Spacer(modifier = Modifier.height(8.dp))
-            TvSeriesGrid(
-                series = series,
-                gridState = gridState,
-                isLoading = isLoading,
-                loadMore = {
-                    viewModel.loadNextPage()
-                }
-            ) { selectedSeries ->
-                val intent = Intent(context, DetailActivity::class.java).apply {
-                    putExtra("SERIES_ID", selectedSeries.id)
-                }
-                context.startActivity(intent)
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(color = Color.Black)
+        .padding(top = 20.dp)
+    ) {
+        AppBar()
+
+        Spacer(modifier = Modifier.height(8.dp))
+        SearchAndSortBar()
+        Spacer(modifier = Modifier.height(8.dp))
+        TvSeriesGrid(
+            series = series,
+            gridState = gridState,
+            isLoading = isLoading,
+            loadMore = {
+                viewModel.loadNextPage()
             }
+        ) { selectedSeries ->
+            val intent = Intent(context, DetailActivity::class.java).apply {
+                putExtra("SERIES_ID", selectedSeries.id)
+            }
+            context.startActivity(intent)
+        }
+        Spacer(modifier = Modifier.weight(1f))
+
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppBar() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Image(
+            painter = rememberAsyncImagePainter(
+                ImageRequest.Builder(LocalContext.current)
+                    .data("https://www.srinathdev.me/img/night.png")
+                    .crossfade(true)
+                    .build()
+            ),
+            contentDescription = "Profile Picture",
+            modifier = Modifier
+                .size(34.dp)
+                .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Box(
+            modifier = Modifier
+                .height(40.dp)
+                .weight(Float.MAX_VALUE)
+                .fillMaxHeight(), // Ensure the Box takes up the remaining height
+            contentAlignment = Alignment.Center // Center-align content within the Box
+        ) {
+            Text(
+                text = "TMB Popular Series",
+                color = Color.White,
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center,
+            )
         }
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SearchBar() {
+fun SearchAndSortBar() {
     var searchQuery by remember { mutableStateOf("") }
 
-    OutlinedTextField(
-        value = searchQuery,
-        onValueChange = { searchQuery = it },
-        label = { Text("Search", color = Color.White) },
+    // Define a common height for both search bar and sort icon
+    val commonHeight = 56.dp
+
+    Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(8.dp),
-        colors = TextFieldDefaults.outlinedTextFieldColors(
-            focusedBorderColor = Color.White,
-            unfocusedBorderColor = Color.Gray,
-            focusedTextColor = Color.White,
-            cursorColor = Color.White,
-            focusedLabelColor = Color.White,
-            unfocusedLabelColor = Color.Gray
+            .padding(top = 0.dp, start = 8.dp, end = 8.dp, bottom = 10.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        OutlinedTextField(
+            value = searchQuery,
+            onValueChange = { searchQuery = it },
+            label = { Text("Search", color = Color.White) },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Search, // Use the built-in search icon
+                    contentDescription = "Search",
+                    tint = Color.White,
+                    modifier = Modifier.size(24.dp) // Adjust size as needed
+                )
+            },
+            modifier = Modifier
+                .weight(1f)
+                .padding(end = 8.dp)
+                .height(commonHeight), // Set the height for the search box
+            shape = RoundedCornerShape(12.dp), // Add rounded corners
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = Color.White,
+                unfocusedBorderColor = Color.Gray,
+                focusedTextColor = Color.White,
+                cursorColor = Color.White,
+                focusedLabelColor = Color.White,
+                unfocusedLabelColor = Color.Gray
+            )
         )
-    )
+        IconButton(
+            onClick = { /* Handle sort click */ },
+            modifier = Modifier.height(commonHeight) // Set the same height for the sort icon
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.baseline_sort_24), // Use the drawable resource
+                contentDescription = "Sort",
+                modifier = Modifier.size(24.dp).align(Alignment.CenterVertically), // Adjust size as needed
+                colorFilter = ColorFilter.tint(Color.White) // Apply tint color
+            )
+        }
+    }
 }
-
 @Composable
 fun TvSeriesGrid(
     series: List<TvSeries>,
@@ -90,27 +174,31 @@ fun TvSeriesGrid(
     loadMore: () -> Unit,
     onItemClick: (TvSeries) -> Unit
 ) {
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(3),
-        state = gridState,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        items(series.size) { index ->
-            TvSeriesItem(tvSeries = series[index], onClick = { onItemClick(series[index]) })
+    Box(modifier = Modifier.fillMaxSize()) {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(3),
+            state = gridState,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            items(series.size) { index ->
+                TvSeriesItem(tvSeries = series[index], onClick = { onItemClick(series[index]) })
+            }
+
+            item {
+                if (isLoading) {
+                    Spacer(modifier = Modifier.height(16.dp)) // Add space to prevent overlap with loading indicator
+                }
+            }
         }
 
-        item {
-            if (isLoading) {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp),
-                    contentAlignment = Alignment.Center
-                ) {
-                    CircularProgressIndicator(color = Color.Red)
-                }
-            } else {
-                Spacer(modifier = Modifier.height(16.dp))
+        if (isLoading) {
+            Box(
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .padding(16.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator(color = Color.Red)
             }
         }
     }
